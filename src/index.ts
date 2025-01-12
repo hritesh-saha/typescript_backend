8853882e793c3b6b38cd320c77a09251b5c80152
Hritesh_Saha <hriteshsaha4@gmail.com>
2025-01-12 21:36:44 +0530

    changing database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express, {Express, Request, Response} from "express"
-
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import { error } from "console";
 const app:Express=express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const server= http.createServer(app);
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("Working");
 })
 
-app.listen(3000,()=>{
+const port = process.env.PORT;
+server.listen(port,()=>{
     console.log("Server is running on port 3000");
-})
+});
+
+const db_uri=process.env.DB_URI;
+
+mongoose.Promise=Promise;
+mongoose.connect(db_uri).then(()=>console.log("Database Connect!"));
+mongoose.connection.on('error', (error:Error)=> console.log(error));
